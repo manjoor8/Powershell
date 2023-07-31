350c0f21f37356832f618755e8728aeb46eb0420
manjoor8 <86738321+manjoor8@users.noreply.github.com>
2023-07-31 13:24:30 +0530

    Create GetWifiPasswordV2.ps1
    shorter version of original code

---
+++ GetWifiPasswordV2.ps1
@@ -0,0 +1,14 @@
+$ssidPasswordArray = @()
+$netshOutput = netsh wlan show profiles
+$ssidNames = $netshOutput | Select-String -Pattern 'All User Profile\s+:\s+(.+)'
+foreach ($ssid in $ssidNames) {
+    $ssidName = $ssid.Matches.Groups[1].Value.Trim()
+    $p = netsh wlan show profile name="$ssidName" key=clear 
+    $p = $p | Select-String -Pattern "Key Content\s+:\s+(.+)"
+    $p = $p.Matches.Groups[1].Value.Trim()
+    $ssidPasswordArray += [PSCustomObject]@{
+        SSID = $ssidName
+        Password = $p
+    }
+ }
+ $ssidPasswordArray | ConvertTo-Json
