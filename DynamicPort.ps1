74d33ceb7c309a0dcaaad1d6667134c88a627940
manjoor8 <86738321+manjoor8@users.noreply.github.com>
2023-10-31 20:58:49 +0530

    Update DynamicPort.ps1

--- DynamicPort.ps1
+++ DynamicPort.ps1
@@ -1,6 +1,6 @@
 # Set port
-netsh int ipv4 set dynamicport tcp start=3000 num=65000
-netsh int ipv6 set dynamicport tcp start=3000 num=65000
+netsh int ipv4 set dynamicport tcp start=3000 num=62000
+netsh int ipv6 set dynamicport tcp start=3000 num=62000
 # Show current setting
 netsh int ipv4 show dynamicport tcp
 netsh int ipv6 show dynamicport tcp
