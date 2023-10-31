cff4c1a8d67ce9cdf82509186375544f92ab82ce
manjoor8 <86738321+manjoor8@users.noreply.github.com>
2023-10-31 19:35:28 +0530

    Create FixNLAError.ps1

---
+++ FixNLAError.ps1
@@ -0,0 +1,3 @@
+//(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices  -Filter "TerminalName='RDP-tcp'").UserAuthenticationRequired
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices  -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices  -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(1)
