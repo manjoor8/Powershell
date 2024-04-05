074776c31fc4b85c42faa816c0f09c68a2778ca1
manjoor8 <86738321+manjoor8@users.noreply.github.com>
2024-04-05 20:05:57 +0530

    Update download-all-blobs-in-a-container.ps1

--- download-all-blobs-in-a-container.ps1
+++ download-all-blobs-in-a-container.ps1
@@ -1,3 +1,2 @@
 $storage_account = New-AzureStorageContext -ConnectionString '<Storage-Connection-String>'
-$Blobs = Get-AzureStorageBlob -Container '<Container_Name>' -Context $storage_account | Where-Object { $_.Name.ToLower().EndsWith(".txt") } # <= Filter is optional
-$blobs | Get-AzureStorageBlobContent -Destination 'c:\temp\files' -Force
+$Blobs = Get-AzureStorageBlob -Container '<Container_Name>' -Context $storage_account | Where-Object { $_.Name.ToLower().EndsWith(".txt") } | Get-AzureStorageBlobContent -Destination 'c:\temp\files' -Force # <= Where Filter is optional
