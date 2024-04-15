05b29089add2c6bc5b88714ca2584dca9a0fe68f
manjoor8 <86738321+manjoor8@users.noreply.github.com>
2024-04-15 14:25:57 +0530

    Create IssueWildcardCertificate.ps1

---
+++ IssueWildcardCertificate.ps1
@@ -0,0 +1,5 @@
+#Install Posh-ACME if not already installed
+#Install-Module -Name Posh-ACME
+New-PACertificate *.yourdomain.com -AcceptTOS -Contact <your-email> -DnsPlugin AcmeDns -PluginArgs @{ACMEServer='auth.acme-dns.io'} -Install -PfxPass <pfx-Password>
+It will prompt you to create CNMAE record. Create the record and hit any key after 2 minutes. Wait for few minutes
+The pfx will be located in the folder %LOCALAPPDATA%\Posh-ACME.
